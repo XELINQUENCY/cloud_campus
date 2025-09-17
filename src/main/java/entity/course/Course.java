9693fb1c9e65cec481e15dbd9972ca6356e178a2
Illunight <xelinquency@outlook.com>
2025-09-17 22:32:33 +0800

    course实体类

--- src/main/java/entity/course/Course.java
+++ src/main/java/entity/course/Course.java
@@ -8,4 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Course {
+    String courseId;
+    String courseName;
+    String department;
+    double credit;
 }
