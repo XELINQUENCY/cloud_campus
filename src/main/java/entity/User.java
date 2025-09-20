32d80b258c53a6fd5190f5abc977eee741fa29d6
Illunight <xelinquency@outlook.com>
2025-09-20 23:36:48 +0800

    不会再修改了，这就是成品

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,7 +1,9 @@
 package entity;
 
 import enums.UserRole;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,8 @@ import java.util.Set;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String id;
     private String name;
